948865621b05cb89c01a569d85d8c4a1ab039ff7
akshay0570 <100024873+Akshay0570@users.noreply.github.com>
2024-06-19 12:03:40 +0530

    Create variable.tf

---
+++ Terraform/variable.tf
@@ -0,0 +1,45 @@
+variable "resource_group_name" {
+  default     = "demo-rg-01"
+  description = "This resource group is used to create resource for demo"
+  type        = string
+}
+variable "item_count" {
+  description = "Number of instance to create"
+  type        = number
+  default     = 3
+}
+
+variable "location" {
+  default = "South India"
+  type    = string
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "demo-vnet"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "demo-subnet"
+}
+
+variable "publicip_name" {
+  type    = string
+  default = "demo-public-ip-02"
+}
+
+variable "nsg_name" {
+  type = string
+  default = "demo-nsg-01"
+}
+
+variable "nic_name" {
+  type = string
+  default = "demo-nic"
+}
+
+variable "linux_vm" {
+  type = string
+  default = "demo-linux-vm"
+}
